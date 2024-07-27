072078de1b30cc2b3d26bf9bad910ca069fbccc0
Ruslan <ruslanishmaev03@gmail.com>
2024-07-27 16:07:58 +0300

    config server

--- configuration-server/src/main/java/ru/voltjunkie/configurationserver/ConfigurationApplication.java
+++ configuration-server/src/main/java/ru/voltjunkie/configurationserver/ConfigurationApplication.java
@@ -1,4 +1,15 @@
 package ru.voltjunkie.configurationserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigurationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationApplication.class, args);
+    }
 }
