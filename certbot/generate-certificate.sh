30fb400ac8d2978c56e57dd638dee46ed17a8a19
Ruslan <ruslanishmaev03@gmail.com>
2024-08-09 12:28:35 +0300

    ssl-certificate

---
+++ certbot/generate-certificate.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+# generate-certificate.sh
+
+rm -rf /etc/letsencrypt/live/certfolder*
+
+certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=certfolder --key_type rsa --agree-tos
+
+
+rm -rf /etc/nginx/cert.pem
+rm -rf /etc/nginx/key.pem
+
+cp /etc/letsencrypt/live/certfolder*/fullchain.pem /etc/nginx/cert.pem
+cp /etc/letsencrypt/live/certfolder*/privkey.pem /etc/nginx/key.pem
